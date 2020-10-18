5e2807dba8747b35ac25ea8d4994ee3e17e66544
brisibe <43288228+jbrisibe10@users.noreply.github.com>
2020-10-18 23:13:16 +0100

    Add project files.

---
+++ ShopBeta.Api/Extensions/ServiceExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShopBeta.Core.Interfaces;
+using ShopBeta.Infrastructure.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopBeta.Api.Extensions
+{
+    public static class ServiceExtension
+    {
+
+        public static void ConfigureCors(this IServiceCollection services) =>
+                services.AddCors(options =>
+                {
+                    options.AddPolicy("CorsPolicy", builder =>
+                        builder.WithOrigins("https://localhost:3000")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+
+                });
+
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+    }
+}
